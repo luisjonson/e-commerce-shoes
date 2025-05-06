b27994c615e8a09f36b4c72ae676fb69a9549e52
Jonson <luizlindonjonson@yahoo.com.br>
2025-05-05 23:59:54 -0300

    Melhorando o card

--- README.md
+++ README.md
@@ -22,6 +22,9 @@ If you are developing a production application, we recommend using TypeScript wi
 
 # Biblioteca
 ## npm install styled-components
+## primereact 
+## primeflex 
+## primeicons
 
 **Bibliotecara ultilizado no carocel**
 ## npm install swiper
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,50 +1,95 @@
 import React from 'react'
 import styled from 'styled-components';
+import tenis from '../assets/tenis_template.svg'
 
 const CardConteiner = styled.div`
-      width: 292px;
-      height: auto;
-      background-color: #fff;
-      border-radius: 8px;
-      box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.1);
-      overflow: hidden;
-      margin: 20px;
-      font-family: sans-serif;
-
-    & .conteudo {
-    padding: 16px;
-    background-color: #f44336; // vermelho suave
-    color: white;
+  width: 292px;
+  height: 439px;
+  border-radius: 4px;
+  overflow: hidden;
+  margin: 50px;
+  font-family: sans-serif;
+  display: flex;
+  flex-direction: column;
+  
+  & .conteudo {
+    width: 292px;
+    height: 321px;
+    background-color: var(--white); // vermelho suave
+    color: var(--dark);
     font-size: 20px;
-    font-weight: bold;
-  }
+    &.conteudo img{
+      width: 250px;
+      height: 240px;
+    }
+    & .desconto{
+      margin:1rem;
+      width: 96px;
+      height: 32px;
+      display: flex;
+      justify-content: center;
+      border-radius: 29px;
+      align-items: center;
+      background-color: var(--green-limon);
+
+      &.desconto p{
+        margin: 0;
+        font-size: 12px;
+      }
+    }
     
-  #informacao {
-    padding: 16px;
-    background-color: #0a77d6;
-    color: white;
-
-    h2 {
-      margin: 0 0 8px 0;
+  } 
+  & .informacao {
+    /* padding: 16px; */
+    &.informacao  #tipo{
+      width: 35px;
+      height: 24px;
+      color: var(--light-gray);
+    }
+    
+    &.informacao h3{
+      font-size: 24px;
+      color: var(--dark-gray-2-rgba);
+    }
+   
+    & .valor{
+      gap: 1rem;
+      display: flex;
+      align-items: flex-end;
+      &.valor #descoto{
+        text-decoration: line-through;
+        font-size: 1.5rem;
+        color: var(--light-gray);
+      }
+      
+      &.valor #valordesconto{
+        font-size: 2rem;
+        font-weight: bold;
+        color: var(--dark-gray);
+      }
     }
 
-    h3 {
-      margin: 0;
-      font-weight: normal;
-    }
-    }
-
+    
+  }
 `
 
 const Card = () => {
   return (
     <CardConteiner>
+      <h4>Produto em alta</h4>
       <div className="conteudo">
-        <h1>Conteudo</h1>
+        <div className='desconto'>
+          <p>30% OFF</p>
+        </div>
+        <img src={tenis} alt='tenis'></img>
       </div>
-      <div id="informacao">
-        <h2>Tênis</h2>
-        <h3>K-Swiss V8 -</h3>
+      <div className="informacao">
+        <p id='tipo'>Tênis</p>
+        <h3>K-Swiss V8 - Masculino</h3>
+        <div className='valor'>
+          <p id='descoto'>$200</p>
+          <p id='valordesconto'>$100</p>
+        </div>
       </div>
 
     </CardConteiner>
