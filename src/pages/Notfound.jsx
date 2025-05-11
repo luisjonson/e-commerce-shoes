cab3e2568f695527c80f1e05ab4ab350185aa83e
Jonson <luizlindonjonson@yahoo.com.br>
2025-05-11 12:45:43 -0300

    melhorando a pagina de notfound

--- src/pages/Notfound.jsx
+++ src/pages/Notfound.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import voltar from '../assets/voltar.svg'
+
+const NotfoundStyled = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    font-family: var(--font-family-padrao);
+    min-height: 100vh;
+
+    & span{
+        font-size: 5rem;
+    }
+
+    & a{
+        padding-top: 20px;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+    }
+
+    & #voltar {
+        width: 50px;
+        padding: 0;
+    }
+`
 
 function Notfound() {
     return (
-        <div>
-            <h3>Pagina não encontrada</h3>
-            <Link to={"/"}>Voltar para a Home</Link>
-        </div>
+        <NotfoundStyled>
+            <span>404</span>
+            <strong>File not found</strong>
+            <Link to={"/"}> <img id='voltar' src={voltar} alt="voltar" />Voltar para a Home</Link>
+        </NotfoundStyled>
     )
 }
 
