5d164bdc758b24c5d1af941e23b4a343dc6b473e
Jonson <luizlindonjonson@yahoo.com.br>
2025-05-11 22:29:49 -0300

    colocando a obrigatoriedade nos inputs.

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -12,10 +12,10 @@ const Cadastro = () => {
     <>
       <form onSubmit={handerSubimit}>
         <FormCard title="Cadastro">
-          <InputWithLabel label='Nome' type="text" />
-          <InputWithLabel label='E-mail' type="email" />
-          <InputWithLabel label='Senha' type="password" />
-          <InputWithLabel label='Confirmar senha' type="password" />
+          <InputWithLabel label='Nome' type="text" required/>
+          <InputWithLabel label='E-mail' type="email" required/>
+          <InputWithLabel label='Senha' type="password" required/>
+          <InputWithLabel label='Confirmar senha' type="password" required/>
           <InputWithLabel label='Eu aceito os termos de uso e políticas de privacidade ' type="checkbox" required/>
           <button type='submit' style={{width: '100%'}} className='primary'>Cadastrar</button>
         </FormCard>
