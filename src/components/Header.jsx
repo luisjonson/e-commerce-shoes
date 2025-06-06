f01cebf8f2834d2f3809634a92042b945c733219
Jonson <luizlindonjonson@yahoo.com.br>
2025-06-05 23:23:35 -0300

    Acresentando a mensagem de sucesso apos o login

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,20 @@ import carrinho from '../assets/mini-cart.svg'
 import search from '../assets/Search.svg'
 import { useNavigate } from 'react-router-dom';
 import '../css/Header.css'
+import MsgSucess from './msg_alert/MsgSucess';
+import { useLocation } from 'react-router-dom';
 
 const Header = () => {
+const location = useLocation();
+const mensagem = location.state?.msg;
+
   const navigate = useNavigate();
   const handleLogin = () => {
     navigate('login')
   }
   return (
     <>
+    { mensagem && <MsgSucess msg={mensagem}/>}
       <div className='container'>
         <div className='search'>
           <img src={logo} alt="Logo" />
