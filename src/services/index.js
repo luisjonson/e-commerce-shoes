5c1cf9eeca2121b7ce83ccbf31694276126d9478
Jonson <luizlindonjonson@yahoo.com.br>
2025-06-10 20:29:32 -0300

    configurando variavel de ambiente no front.

--- src/services/index.js
+++ src/services/index.js
@@ -9,7 +9,7 @@ export const API = axios.create({
 
 
 export const APIBACKEND = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL:  import.meta.env.VITE_API_URL,
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite"
+import react from "@vitejs/plugin-react"
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/e-commerce-shoes',
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "")
+
+  return {
+    base: "/e-commerce-shoes",
+    plugins: [react()],
+    server: {
+      host: env.VITE_HOST || "0.0.0.0",
+      port: Number.parseInt(env.VITE_PORT || "5173"),
+    },
   }
 })
