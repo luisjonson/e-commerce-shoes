409a4e782b4e0ec231619cdd7fef7dd1d441a350
luisjonson <luizlindonjonson@yahoo.com.br>
2025-06-22 23:05:45 -0300

    fefatorando melho o selectMenu

--- src/components/SelectMenu.jsx
+++ src/components/SelectMenu.jsx
@@ -2,23 +2,41 @@ import styled from "styled-components";
 
 const SelectMenuStyled = styled.select`
   padding: 8px 12px;
+  margin-bottom: 15px;
   border-radius: 5px;
   border: 1px solid #ccc;
   font-size: 16px;
-  width: 300px;
+  width: 100%;
   background: transparent;
 `;
 
-const SelectMenu = ({ colecao, label, onChange, optionKey = "id", optionValue = "nome", value}) => {
+const SelectMenu = ({
+  colecao,
+  label,
+  onChange,
+  optionKey = "id",
+  optionValue = "nome",
+  value,
+  required,
+}) => {
   return (
     <>
-        <label>{label}</label>    
-      <SelectMenuStyled id={label}  onChange={onChange} value={value}>
+      <label>
+        {label}
+        {required && <span className="required-asterisk">*</span>}
+      </label>
+      <SelectMenuStyled
+        id={label}
+        onChange={onChange}
+        value={value}
+        required={required}
+      >
+        <option value="">Selecione...</option>
         {colecao?.map((item) => (
-            <option key={item[optionKey]} value={item[optionKey]}>
-              {item[optionValue]}
-            </option>
-          ))}
+          <option key={item[optionKey]} value={item[optionKey]}>
+            {item[optionValue]}
+          </option>
+        ))}
       </SelectMenuStyled>
     </>
   );
