cea49f574dd8d9147a0a4507443d915cadcafa68
luisjonson <luizlindonjonson@yahoo.com.br>
2025-06-22 21:49:37 -0300

    criando a formação de moeda e removendo a formatação.

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,11 +1,23 @@
 export function capitalizeWords(text) {
   return text
     .toLowerCase()
-    .split(' ')
-    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(' ');
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
 }
 
-export function porcentagemDesconto (preco,  precoPromocional) {
+export function porcentagemDesconto(preco, precoPromocional) {
   return Math.round(((preco - precoPromocional) / preco) * 100);
-};
+}
+
+export function formatarMoeda(valor) {
+  const numero = parseInt(valor.replace(/\D/g, ""), 10) / 100;
+  return numero.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+}
+
+export function parseFloatComMoeda(valor) {
+  return parseFloat(valor.replace(/[^0-9,-]+/g, "").replace(",", "."));
+}
