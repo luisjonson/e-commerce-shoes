9ce4252f71287bb5ed403386992b74fcb00e337a
luisjonson <luizlindonjonson@yahoo.com.br>
2025-06-22 21:50:38 -0300

    cadastro de prduto.

--- src/pages/CadastroProduto.jsx
+++ src/pages/CadastroProduto.jsx
@@ -8,6 +8,9 @@ import CadastroProdutoService from "../services/ProdutoServer";
 import SelectMenu from "../components/SelectMenu";
 import CategoriaServer from "../services/CategoriaServer";
 import MarcaServer from "../services/MarcaServer";
+import { Generos } from '../utils/Generos'
+import { Estados } from '../utils/Estados'
+import {formatarMoeda, parseFloatComMoeda} from "../utils/Utils";
 
 const CadastroProduto = () => {
   const [title, setTitle] = useState("");
@@ -23,6 +26,8 @@ const CadastroProduto = () => {
   const [msgSucess, setMsgSucess] = useState("");
   const [categorais, setCategorias] = useState([]);
   const [marcas, setMarcas] = useState([]);
+  const [genero, setGenero] = useState("");
+  const [estadoProduto, setEstadoProduto] = useState("");
 
   async function buscarCategoria() {
     const categorais = await CategoriaServer.findAll();
@@ -39,39 +44,67 @@ const CadastroProduto = () => {
     buscarMarca();
   }, []);
 
-  //  const isCheckbox = type === "checkbox";
-
-  const handerSubimit = async (e) => {
+  const handerSubmit = async (e) => {
     e.preventDefault();
     const produto = {
-      title: title,
-      valor: valor,
-      valorDesconto: valorDesconto,
+      titulo: title,
+      preco: parseFloat(parseFloatComMoeda(valor)),
+      valorDesconto: parseFloat(parseFloatComMoeda(valorDesconto)),
       descricao: descricao,
       queimaEstoque: queimaEstoque,
       colecao: colecao,
       linkImagem: linkImagem,
       categoria: categoria,
       marca: marca,
+      genero: genero,
+      estadoProduto: estadoProduto,
     };
 
     try {
       console.log(produto);
       const dados = await CadastroProdutoService.criar(produto);
-      setMsgSucess(dados.message);
+      console.log(dados.data);
+      setMsgSucess(dados.data.message);
       setMsgError("");
+      limparCampos();
     } catch (error) {
-      const msg = error.response.data.error;
-      setMsgError(msg);
-      setMsgSucess("");
+    // Tenta pegar mensagem de erro do response se existir
+    const msg = error.response?.data?.message || error.message || "Erro ao cadastrar produto.";
+    setMsgError(msg);
+    setMsgSucess("");
     }
+  };
+
+  const limparCampos = () => {
+    setTitle(""); 
+    setValor("");
+    setValorDesconto("");
+    setDescricao("");
+    setQueimaEstoque(false);
+    setColecao(false);
+    setLinkImagem("");
+    setCategoria("");
+    setMarca("");
+    setGenero("");
+    setEstadoProduto("");
+  };
+
+  const handleChangeValor = (e) => {
+    const valorDigitado = e.target.value;
+    const valorFormatado = formatarMoeda(valorDigitado);
+    setValor(valorFormatado);
+  };
+  const handleChangevalorDesconto = (e) => {
+    const valorDigitado = e.target.value;
+    const valorFormatado = formatarMoeda(valorDigitado);
+    setValorDesconto(valorFormatado);
   };
 
   return (
     <>
       {msgError && <MsgError msg={msgError} />}
       {msgSucess && <MsgSucess msg={msgSucess} />}
-      <form onSubmit={handerSubimit}>
+      <form onSubmit={handerSubmit}>
         <FormCard title="Cadastro Produto">
           <InputWithLabel
             label="Title"
@@ -84,14 +117,14 @@ const CadastroProduto = () => {
             label="Valor"
             type="text"
             value={valor}
-            onChange={(e) => setValor(e.target.value)}
+            onChange={handleChangeValor}
             required
           />
           <InputWithLabel
             label="Valor desconto"
             type="text"
             value={valorDesconto}
-            onChange={(e) => setValorDesconto(e.target.value)}
+            onChange={handleChangevalorDesconto}
             required
           />
           <InputWithLabel
@@ -102,15 +135,42 @@ const CadastroProduto = () => {
             required
           />
           <SelectMenu
-            colecao={categorais}
+            colecao={categorais.map(cat => ({
+              id: cat.numsequencial,
+              nome: cat.nome
+            }))}
+            optionKey="id"
+            optionValue="nome"
+            value={categoria}
             onChange={(e) => setCategoria(e.target.value)}
             label="Categorias"
           />
           <SelectMenu
-            colecao={marcas}
+            colecao={marcas.map(cat => ({
+              id: cat.numsequencial,
+              nome: cat.nome
+            }))}
+            optionKey="id"
+            optionValue="nome"
             value={marca}
             onChange={(e) => setMarca(e.target.value)}
             label="Marcas"
+          />
+          <SelectMenu
+            colecao={Object.entries(Generos).map(([key, value]) => ({ numsequencial: key, nome: value }))}
+            optionKey="numsequencial"
+            optionValue="nome"
+            value={genero}
+            onChange={(e) => setGenero(e.target.value)}
+            label="Gêneros"
+          />
+          <SelectMenu
+            colecao={Object.entries(Estados).map(([key, value]) => ({ numsequencial: key, nome: value }))}
+            optionKey="numsequencial"
+            optionValue="nome"
+            value={estadoProduto}
+            onChange={(e) => setEstadoProduto(e.target.value)}
+            label="Gêneros"
           />
           <InputWithLabel
             label="Queima de estoque"
