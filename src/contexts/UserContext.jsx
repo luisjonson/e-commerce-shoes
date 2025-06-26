e79d7312a544d29f069a905be68df3ea154db177
luisjonson <luizlindonjonson@yahoo.com.br>
2025-06-25 23:03:31 -0300

    Corrigindo o userContext

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -10,12 +10,39 @@ const UserContext = createContext();
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [msgError, setMsgError] = useState("");
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const carregarUsuario = async () => {
+      try {
+        setLoading(true);
+        const token = Cookies.get("token");
+        
+        if (token) {
+          const dados = await auth.usuarioLogado();
+          setUser(dados?.data);
+        }
+      } catch (error) {
+        console.error("Erro ao carregar usuário:", error);
+        setMsgError(error.response?.data?.erro || "Erro ao carregar usuário.");
+        Cookies.remove("token");
+        setUser(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    carregarUsuario();
+  }, []); 
 
   const login = async (userData) => {
     try {
+      setLoading(true);
       await auth.login(userData); // autentica e backend já seta o cookie
       const dados = await auth.usuarioLogado(); // pega usuário autenticado
+      console.log("Usuário autenticado:", dados.data);
+    
       setUser(dados.data);
       setMsgError("");
       navigate("/", {
@@ -24,35 +51,29 @@ export const UserProvider = ({ children }) => {
     } catch (error) {
       console.error("Erro ao fazer login:", error);
       setMsgError(error.response?.data?.erro || "Erro ao fazer login.");
+    } finally {
+      setLoading(false);
     }
   };
 
   const logout = () => {
     setUser(null);
-    navigate("/login");
+    setMsgError("");
     Cookies.remove("token");
+    navigate("/");
   };
 
-  useEffect(() => {
-    const carregarUsuario = async () => {
-      try {
-        const dados = await auth.usuarioLogado();
-        setUser(dados.data);
-      } catch (error) {
-        setMsgError(error)
-        Cookies.remove("token");
-      }
-    };
-
-    carregarUsuario();
-  }, []);
-
   return (
-    <UserContext.Provider value={{ user, login, logout, msgError }}>
+    <UserContext.Provider value={{ 
+      user, 
+      login, 
+      logout, 
+      msgError, 
+      loading 
+    }}>
       {children}
     </UserContext.Provider>
   );
 };
 
-// Hook para usar o contexto
-export const useUser = () => useContext(UserContext);
+export const UseUser = () => useContext(UserContext);
