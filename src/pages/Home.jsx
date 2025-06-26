fa682b3125aa926f1f0ec09c9dba9eb260ee9c2f
luisjonson <luizlindonjonson@yahoo.com.br>
2025-06-25 23:11:16 -0300

    refatorando a home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
-import { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import Card_oferta from '../components/Card_oferta';
-import Card_slider from '../components/Card_slider';
-import CardProduto from '../components/CardProduto';
-import Carousel from '../components/Carousel';
-import Colecao from '../components/Colecao';
-import produtosServer from '../services/ProdutoServer';
-import { porcentagemDesconto } from '../utils/Utils';
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import Card_oferta from "../components/Card_oferta";
+import Card_slider from "../components/Card_slider";
+import CardProduto from "../components/CardProduto";
+import Carousel from "../components/Carousel";
+import Colecao from "../components/Colecao";
+import produtosServer from "../services/ProdutoServer";
+import { porcentagemDesconto } from "../utils/Utils";
 
 const HomeStaled = styled.div`
   width: 100%;
@@ -69,8 +69,14 @@ function Home() {
   const [produtos, setProdutos] = useState([]);
 
   async function buscarProdutos() {
-    const request = await produtosServer.findAll()
-    setProdutos(request.data.data);
+    try {
+      const request = await produtosServer.findAll();
+      setProdutos(request.data.data);
+
+    } catch (error) {
+      console.error("Erro ao buscar produtos:", error);
+      setProdutos([]);
+    }
   }
 
   useEffect(() => {
@@ -78,42 +84,49 @@ function Home() {
   }, []);
 
   return (
-    <HomeStaled >
-
+    <HomeStaled>
       {produtos.length > 0 && (
-        <Carousel settings={{
-          slidesPerView: 1,
-          spaceBetween: 20,
-          navigation: true,
-          pagination: { clickable: false },
-          loop: true
-        }}>
-
-        {produtos.filter(produto => produto.queimaEstoque).map((produto) => (
-          <div key={produto.numsequencial}>
-              <Card_slider
-                text={produto.titulo}
-                titulo={produto.titulo}
-                descricao={produto.descricao}
-                imag1={produto.linkImagem}
-                imag2={produto.linkImagem}
-              />
-          </div>
-        ))}
+        <Carousel
+          settings={{
+            slidesPerView: 1,
+            spaceBetween: 20,
+            navigation: true,
+            pagination: { clickable: false },
+            loop: true,
+          }}
+        >
+          {produtos
+            .filter((produto) => produto.queimaEstoque)
+            .map((produto) => (
+              <div key={produto.numsequencial}>
+                <Card_slider
+                  text={produto.titulo}
+                  titulo={produto.titulo}
+                  descricao={produto.descricao}
+                  imag1={produto.linkImagem}
+                  imag2={produto.linkImagem}
+                />
+              </div>
+            ))}
         </Carousel>
       )}
 
       {produtos.length > 0 && (
         <section className="section-destaque">
           <h4 className="titulo">Coleções em destaque</h4>
-          <ul >
-            {produtos.filter(prod => prod.queimaEstoque === true)
-            .slice(0,3)
-            .map((produto) => (
-              <div key={produto.numsequencial}>
-                <Card_oferta oferta={produto.preco} img={produto.linkImagem} alt={produto.titulo} />
-              </div>
-            ))}
+          <ul>
+            {produtos
+              .filter((prod) => prod.queimaEstoque === true)
+              .slice(0, 3)
+              .map((produto) => (
+                <div key={produto.numsequencial}>
+                  <Card_oferta
+                    oferta={produto.preco}
+                    img={produto.linkImagem}
+                    alt={produto.titulo}
+                  />
+                </div>
+              ))}
           </ul>
         </section>
       )}
@@ -121,14 +134,17 @@ function Home() {
       <Colecao />
 
       {produtos.length > 0 && (
-        <section className='section-destaque'>
-          <h4 className='titulo'>Produtos em alta</h4>
+        <section className="section-destaque">
+          <h4 className="titulo">Produtos em alta</h4>
           <ul>
             {produtos.map((produto) => (
               <div key={produto.numsequencial}>
-                <CardProduto  
-                  desconto={ produto.precoPromocional && porcentagemDesconto(produto.preco , produto.precoPromocional)}
-                  image={produto.linkImagem} 
+                <CardProduto
+                  desconto={
+                    produto.precoPromocional &&
+                    porcentagemDesconto(produto.preco, produto.precoPromocional)
+                  }
+                  image={produto.linkImagem}
                   category={produto.category}
                   title={produto.titulo}
                   price={produto.preco}
@@ -139,9 +155,8 @@ function Home() {
           </ul>
         </section>
       )}
-
     </HomeStaled>
-  )
+  );
 }
 
-export default Home
+export default Home;
