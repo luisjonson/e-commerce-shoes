c781b768b093c8e933cce2de7984b6a37f94ea86
luisjonson <luizlindonjonson@yahoo.com.br>
2025-06-25 23:13:18 -0300

    colcando um interceptor pava validação de token

--- src/services/index.js
+++ src/services/index.js
@@ -1,18 +1,32 @@
-import axios from 'axios'
+import axios from "axios";
+import Cookies from "js-cookie";
 
 export const API = axios.create({
-    baseURL: 'https://fakestoreapi.com',
-    headers: {
-        'Content-Type': 'application/json'
-    }
-})
-
+  baseURL: "https://fakestoreapi.com",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
 export const APIBACKEND = axios.create({
-    baseURL:  import.meta.env.VITE_API_URL,
-    withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json'
-    }
-})
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
+// Intercepta erros globais da API
+APIBACKEND.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      console.warn("Token expirado ou inválido. Desconectando...");
+
+      Cookies.remove("user");
+      window.location.href = "/e-commerce-shoes"; // redireciona manualmente
+    }
+
+    return Promise.reject(error);
+  }
+);
